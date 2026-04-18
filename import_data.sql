5890ce4e7b5dc1dae0f875c755d15ff5cd85861e
arcasoyece <arcasoyece@gmail.com>
2026-04-19 00:41:12 +0300

    olist_database-analysis> olist-final_analysis>olist_prefinal_analysis olist e-ticaret analizi tamamlandı. Zaman serisi, heatmap ve kategori bazlı ciro analizleri eklendi.

--- import_data.sql
+++ import_data.sql
@@ -1,52 +1,52 @@
 
 /* importing geo_location data */
-COPY geo_location
-FROM '/home/nabeel/Github/olist_database_analysis/olist_data/olist_geolocation_dataset.csv'
+\copy geo_location
+FROM '/home/ece/code/arcasoyece/olist_database_analysis/data/olist_geolocation_dataset.csv'
 WITH (FORMAT CSV, HEADER);
 
 
 
 /* importing customers data */
-COPY customers
-FROM '/home/nabeel/Github/olist_database_analysis/olist_data/olist_customers_dataset.csv'
+\copy customers
+FROM '/home/ece/code/arcasoyece/olist_database_analysis/data/olist_customers_dataset.csv'
 WITH (FORMAT CSV, HEADER);
 
 
 /* importing sellers data */
-COPY sellers
-FROM '/home/nabeel/Github/olist_database_analysis/olist_data/olist_sellers_dataset.csv'
+\copy sellers
+FROM '/home/ece/code/arcasoyece/olist_database_analysis/data/olist_sellers_dataset.csv'
 WITH (FORMAT CSV, HEADER);
 
 
 
 /* importing products data */
-COPY products
-FROM '/home/nabeel/Github/olist_database_analysis/olist_data/olist_products_dataset.csv'
+\copy products
+FROM '/home/ece/code/arcasoyece/olist_database_analysis/data/olist_products_dataset.csv'
 WITH (FORMAT CSV, HEADER);
 
 
 /* importing orders data */
-COPY orders
-FROM '/home/nabeel/Github/olist_database_analysis/olist_data/olist_orders_dataset.csv'
+\copy orders
+FROM '/home/ece/code/arcasoyece/olist_database_analysis/data/olist_orders_dataset.csv'
 WITH (FORMAT CSV, HEADER);
 
 /* importing order payments data */
-COPY order_payments
-FROM '/home/nabeel/Github/olist_database_analysis/olist_data/olist_order_payments_dataset.csv'
+\copy order_payments
+FROM '/home/ece/code/arcasoyece/olist_database_analysis/data/olist_order_payments_dataset.csv'
 WITH (FORMAT CSV, HEADER);
 
 
 /* importing order reviews data */
-COPY order_reviews
-FROM '/home/nabeel/Github/olist_database_analysis/olist_data/olist_order_reviews_dataset.csv'
+\copy order_reviews
+FROM '/home/ece/code/arcasoyece/olist_database_analysis/data/olist_order_reviews_dataset.csv'
 WITH (FORMAT CSV, HEADER);
 
 /* importing order items data */
-COPY order_items
-FROM '/home/nabeel/Github/olist_database_analysis/olist_data/olist_order_items_dataset.csv'
+\copy order_items
+FROM '/home/ece/code/arcasoyece/olist_database_analysis/data/olist_order_items_dataset.csv'
 WITH (FORMAT CSV, HEADER);
 
 /* importing product translation */
-COPY product_translation
-FROM '/home/nabeel/Github/olist_database_analysis/olist_data/product_category_name_translation.csv'
+\copy product_translation
+FROM '/home/ece/code/arcasoyece/olist_database_analysis/data/product_category_name_translation.csv'
 WITH (FORMAT CSV, HEADER);
